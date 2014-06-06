4eb31145584175ed0b07a51e2d4a794ab1e10ebe
Chaitanya Akkineni <cakkineni@gmail.com>
2014-06-06 11:27:12 -0500

    Changing metrics log to etcd

--- scripts/collect_metrics.sh
+++ scripts/collect_metrics.sh
@@ -1,10 +1,11 @@
 #! /bin/sh
 
-outfile=/var/panamax/metrics/metrics.log
+
 loadAvg="$(awk '{printf $1}' /proc/loadavg)"
 memoryFree="$(gawk '( $1 == "MemFree:" ) { print $2*1024/1000000 }' /proc/meminfo)"
 cpuIdle="$(top -b  -d 0.1 -n 2 | grep '^Cpu(s):' | tail -1 | awk -F"," '{print $4}'|sed 's/%.*//'i)"
 curDate="$(date +'%Y/%m/%d %H:%M:%S')"
-echo "{\"load_average\":\"${loadAvg}\", \"cpu_idle\": \"${cpuIdle}\", \"memory_free\":\"${memoryFree}\", \"date_time\":\"${curDate}\"}" > $outfile
+cpuCores="`nproc`"
+etcdctl set /_panamax/host/metrics "{\"load_average\":\"${loadAvg}\", \"cpu_idle\": \"${cpuIdle}\", \"memory_free\":\"${memoryFree}\",\"cpu_cores\":\"${cpuCores}\",\"last_updated\":\"${curDate}\"}"
 
 exit 0
