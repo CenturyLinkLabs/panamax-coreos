d62f740ef3bf7c2c7cea02f4ace271b66cad786f
Chaitanya Akkineni <cakkineni@gmail.com>
2014-08-11 17:23:07 -0500

    Updating ubuntu.sh to force ln

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,7 +4,7 @@ installer='pmx-installer-latest.zip'
 destination=~/.panamax
 curl -O "http://download.panamax.io/installer/$installer"
 unzip  -ou ${installer}  -d ${destination}
-sudo ln -s ~/.panamax/panamax /usr/local/bin/panamax
+sudo ln -sf ~/.panamax/panamax /usr/local/bin/panamax
 panamax init
 #echo "Execute panamax and select to continue."
 
