856af675e2178a6f2515d11c2ec215dce909e861
root <root@UT1CLABPXE01.local>
2014-10-02 15:11:25 -0700

    Updating to version 0.2.3

--- panamax.rb
+++ panamax.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Panamax < Formula
   homepage "http://www.centurylinklabs.com"
-  url "http://download.panamax.io/installer/pmx-installer-0.2.2.zip"
-  sha1 "b5b00ef1ca37d27dd97d35ebb4ec9280ee8a29d5"
+  url "http://download.panamax.io/installer/pmx-installer-0.2.3.zip"
+  sha1 "0af40e6e597a21c22dcdf1fd5120597a74ed290e"
 
   def install
     cachedir = HOMEBREW_CACHE + "panamax-#{version}.zip"
