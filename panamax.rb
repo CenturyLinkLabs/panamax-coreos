d9e0b9818561b10c81570d63946a93c9c489de63
root <root@UT1CLABPXE01.local>
2014-10-07 08:15:55 -0700

    Updating to version 0.3.0

--- panamax.rb
+++ panamax.rb
@@ -3,8 +3,8 @@ require "formula"
 
 class Panamax < Formula
   homepage "http://www.centurylinklabs.com"
-  url "http://download.panamax.io/installer/panamax-0.2.3.zip"
-  sha1 "0af40e6e597a21c22dcdf1fd5120597a74ed290e"
+  url "http://download.panamax.io/installer/panamax-0.3.0.tar.gz"
+  sha1 "d4079e9b5326eb7a5adee581fa246f453bb4ada8"
 
   def install
     system "./configure", "--prefix=#{prefix}", "--var=#{var}/panamax"
