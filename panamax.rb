381e1ae3ae403771c8dc9ca27dbd22b71a8c0a18
Chaitanya Akkineni <cakkineni@gmail.com>
2014-10-13 10:27:32 -0500

    Adding brew tests.

--- panamax.rb
+++ panamax.rb
@@ -2,10 +2,9 @@ require "formula"
 
 
 class Panamax < Formula
-  homepage "http://www.centurylinklabs.com"
+  homepage "http://www.panamax.io"
   url "http://download.panamax.io/installer/panamax-0.3.0.tar.gz"
   sha1 "d4079e9b5326eb7a5adee581fa246f453bb4ada8"
-
   def install
     system "./configure", "--prefix=#{prefix}", "--var=#{var}/panamax"
     system "make", "install"
@@ -16,8 +15,8 @@ class Panamax < Formula
   end
 
   test do
-    installed = File.exist?("#{prefix}/.panamax")
-    assert installed
-    assert_equal 0, $?.exitstatus
+    assert File.exist?("#{prefix}/.panamax")
+    assert File.exist?("#{var}/panamax")
+    assert_match "#{version}", shell_output("#{prefix}/.panamax/panamax -v").strip
   end
 end
