2e425643ac158b492f198d8f6737a28529f4748c
Chaitanya Akkineni <cakkineni@gmail.com>
2014-10-15 15:10:00 -0500

    Updating unzip command for ubuntu script.

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,8 +3,7 @@
 installer='panamax-latest.tar.gz'
 destination=~/.panamax
 curl -O "http://download.panamax.io/installer/$installer"
-unzip  -ou ${installer}  -d ${destination}
+mkdir -p ${destination} && tar -C ${destination} -zxvf ${installer}
 sudo ln -sf ~/.panamax/panamax /usr/local/bin/panamax
 panamax init
 #echo "Execute panamax and select to continue."
-
