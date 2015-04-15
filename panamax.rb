6c5afd3ac3b2f250e9236e302a5e501f73d550c8
root <root@UT1CLABPXE01.local>
2015-04-15 08:52:08 -0700

    Updating to version 0.5.2

--- panamax.rb
+++ panamax.rb
@@ -3,8 +3,8 @@ require "formula"
 
 class Panamax < Formula
   homepage "http://www.panamax.io"
-  url "http://download.panamax.io/installer/panamax-0.5.1.tar.gz"
-  sha1 "9d15be7ae5dedb5512d905e749fddd7a4424628a"
+  url "http://download.panamax.io/installer/panamax-0.5.2.tar.gz"
+  sha1 "fedd6f8252e30e528805e7a8279f63a002040804"
   def install
     system "./configure", "--prefix=#{prefix}", "--var=#{var}/panamax"
     system "make", "install"
