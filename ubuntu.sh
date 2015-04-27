38c56e179035e2bed4bf29f1a14d86c77f78f8af
Chaitanya Akkineni <cakkineni@gmail.com>
2015-04-27 15:29:41 -0500

    Adding cli to ubuntu installer.

--- ubuntu.sh
+++ ubuntu.sh
@@ -5,5 +5,7 @@ destination=~/.panamax
 curl -O "http://download.panamax.io/installer/$installer"
 mkdir -p ${destination} && tar -C ${destination} -zxvf ${installer}
 sudo ln -sf ~/.panamax/panamax /usr/local/bin/panamax
+curl -O http://download.panamax.io/panamaxcli/panamaxcli-linux
+sudo mv panamaxcli-linux /usr/local/bin/pmxcli && chmod 755 /usr/local/bin/pmxcli
 panamax init
 #echo "Execute panamax and select to continue."
